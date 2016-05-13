99bd7606c9af084190aab3d348898f54af66d2a6
vamshi <itsvamshi@users.noreply.github.com>
2016-05-13 07:12:31 +0530

    Update fibonocci_debug.c

--- fibonocci_debug.c
+++ fibonocci_debug.c
@@ -7,7 +7,6 @@ int main()
   printf("\nEnter upto how many digits(>3) you need the Fibonocci series");
   scanf("%d",&count);
   printf("Fibonocci series :\n");
-  printf("I\tJ\tK");
   for(i=0;i<count;)
   {
     if(count==0)
@@ -16,10 +15,10 @@ int main()
     }
     if(i==0)
     {
-printf("\n%d\n\t%d\n\t%d",i,j,(i+j));
+printf("\t%d\t%d\t%d",i,j,(i+j));
 }
 else
-printf("\n%d",(i+j));
+printf("\t%d",(i+j));
 
 temp = i+j;
 i=j;
